9996c182c1b7f136899986e96e25cfc3e16f34cd
Xingming-Li <damianxml@gmail.com>
2026-02-09 14:28:43 +0100

    create_database.py updated

--- create_database.py
+++ create_database.py
@@ -1,24 +1,27 @@
+# Quota exceeded
+import os
+import shutil
+
+from dotenv import load_dotenv
 # from langchain.document_loaders import DirectoryLoader (legacy)
 from langchain_community.document_loaders import DirectoryLoader
+# from langchain_community.vectorstores import Chroma
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 # from langchain.embeddings import OpenAIEmbeddings (legacy)
 from langchain_openai import OpenAIEmbeddings
- 
-from dotenv import load_dotenv
-import openai
-import os
-import shutil
+# import openai
 
 
 # Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
+
 # Set OpenAI API key (unnecessary)
 # openai.api_key = os.environ['OPENAI_API_KEY']
 
-CHROMA_PATH = "chroma"
-DATA_PATH = "data/classic_books"
+CHROMA_PATH = "chroma_val"
+DATA_PATH = "data/val_books"
 
 def main():
     generate_data_store()
@@ -42,26 +45,29 @@ def split_text(documents: list[Document]):
     )
     chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-
-    document = chunks[10]
-    print(document.page_content)
-    print(document.metadata)
+    # Examine the 10th chunk
+    document_f10 = chunks[10]
+    print(document_f10.page_content)
+    print(document_f10.metadata)
+    # Examine the 10th chunk from the bottom
+    document_l10 = chunks[-10]
+    print(document_l10.page_content)
+    print(document_l10.metadata)
 
     return chunks
 
-
 def save_to_chroma(chunks: list[Document]):
-    # Clear out the database first.
+    # Clear out the database first
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
-    # Create a new DB from the documents.
+    # Create a new DB from the documents
     db = Chroma.from_documents(
         chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
     )
+    # Force to save DB
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
-
 
 if __name__ == "__main__":
     main()
