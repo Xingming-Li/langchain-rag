95d7228aa19e8eed24745fc98afa6d23a941dff5
Xingming-Li <damianxml@gmail.com>
2026-03-20 15:38:01 +0100

    query_data.py modified

--- query_data.py
+++ query_data.py
@@ -26,16 +26,18 @@ def main():
     query_text = args.query_text
 
     # Prepare the DB
-    embedding_function = OpenAIEmbeddings()
+    embedding_function = OpenAIEmbeddings(model="text-embedding-3-small")  # The same one used to create the database
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)  # Return type of search: List[Tuple[Document, float]]
     if len(results) == 0 or results[0][1] < 0.7:
         print(f"Unable to find matching results.")
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    print(context_text)
+    '''
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
@@ -46,7 +48,8 @@ def main():
     sources = [doc.metadata.get("source", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
-
+    '''
+    
 
 if __name__ == "__main__":
     main()
