568282dc15f508d5e7d672b7253ed72f00ef4447
Xingming-Li <damianxml@gmail.com>
2026-03-23 12:28:27 +0100

    query_data.py modified

--- query_data.py
+++ query_data.py
@@ -1,9 +1,17 @@
 import argparse
+from dotenv import load_dotenv
 # from dataclasses import dataclass
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+
+
+# Load environment variables (assume that project contains .env file with API keys)
+load_dotenv()
+
+# Set OpenAI API key (unnecessary)
+# openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma_alice"
 
@@ -31,25 +39,24 @@ def main():
 
     # Search the DB
     results = db.similarity_search_with_relevance_scores(query_text, k=3)  # Return type of search: List[Tuple[Document, float]]
-    if len(results) == 0 or results[0][1] < 0.7:
+    if len(results) == 0 or results[0][1] < 0.3:
         print(f"Unable to find matching results.")
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    print(context_text)
-    '''
+    # print(context_text)
+    
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
-
+    # print(prompt)
+    
     model = ChatOpenAI()
     response_text = model.predict(prompt)
 
     sources = [doc.metadata.get("source", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
-    '''
-    
+        
 
 if __name__ == "__main__":
     main()
